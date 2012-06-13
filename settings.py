0ce6388ae91f8b3f893c7ea262409e716327c11c
Will Wu <trihfh@gmail.com>
2012-06-13 12:40:24 -0700

    This is a complete rewrite of Updater using ExtJS

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
-# Django settings for updates project.
+import os
+# Django settings for uproject project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -10,18 +11,20 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'updates'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'updatesuser'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'updatespass'         # Not used with sqlite3.
+DATABASE_NAME = 'william'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'william'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'will123'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Los Angeles'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -33,7 +36,11 @@ SITE_ID = 1
 # to load the internationalization machinery.
 USE_I18N = True
 
-# Absolute path to the directory that holds media.
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = ''
 
@@ -48,28 +55,30 @@ MEDIA_URL = ''
 ADMIN_MEDIA_PREFIX = '/media/'
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '0ld10@u0$@xorrrw)0+8w*x*0-i)=3666$2ub65kad141^!n09'
+SECRET_KEY = 'hnul6--vmb(3j5v!61w^4-vjevy8xm6tqb56#bc216!nw-nl-%'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'updates.urls'
+ROOT_URLCONF = 'uproject.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/falaki/phd/projects/updaterservice/updates/templates'
+    '/home/william/newupdater/uproject/templates'
 )
 
 INSTALLED_APPS = (
@@ -77,9 +86,23 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'updater'
+    'django.contrib.messages',
+    'haystack',
+    'uapp'
+    # Uncomment the next line to enable the admin:
+    # 'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
 )
 
-LOGIN_URL = '/updates/updater/login/'
-LOGOUT_URL = '/updates/updater/logout/'
-LOGIN_REDIRECT_URL = '/updates/updater/'
+HAYSTACK_SITECONF = 'uapp.search_sites'
+#HAYSTACK_SEARCH_ENGINE = 'xapian'
+#HAYSTACK_XAPIAN_PATH = '/home/william/xapian'
+#HAYSTACK_SEARCH_ENGINE = 'simple'
+HAYSTACK_SEARCH_ENGINE = 'whoosh'
+HAYSTACK_WHOOSH_PATH = '/home/william/whoosh'
+
+LOGIN_URL = '/uproject/uapp/login/'
+LOGOUT_URL = '/uproject/uapp/login/'
+LOGIN_REDIRECT_URL = '/uproject/uapp/'
+CACHE_BACKEND = 'db://listclient_cache'
--- urls.py
+++ urls.py
@@ -4,13 +4,39 @@ from django.conf.urls.defaults import *
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('updater.views',
-    (r'^updater/get/(?P<imei>\w*)$', 'get'),
-    (r'^updater/register/', 'register'),
-    (r'^updater/$', 'index'),
-    (r'^updater/add_user/(?P<imei>\w*)$', 'add_user'),
-    (r'^updater/add_app/(?P<app_str>\w*)$', 'add_app'),
-    (r'^updater/logs/$', 'logs'),
+urlpatterns = patterns('uapp.views',
+	(r'^uapp/add_app/(?P<app_str>\w*)$', 'add_app'),
+	(r'^uapp/add_user/(?P<imei>\w*)$', 'add_user'),
+	(r'^uapp/listClients/$', 'listClients'),
+	(r'^uapp/listApps/$', 'listApps'),
+	(r'^uapp/listAppNames/$', 'listAppNames'),
+	(r'^uapp/listGroups/$', 'listGroups'),
+	(r'^uapp/getClientApps/(?P<imei>\w*)$', 'getClientApps'),
+	(r'^uapp/getClientTags/(?P<imei>\w*)$', 'getClientTags'),
+	(r'^uapp/getClientLogs/(?P<imei>\w*)$', 'getClientLogs'),
+	
+	(r'^uapp/deleteClients/$', 'deleteClients'),
+	(r'^uapp/getGroupApps/(?P<groupname>\w*)$', 'getGroupApps'),
+	(r'^uapp/add_group/$', 'add_group'),
+	(r'^uapp/delete_group/$', 'delete_group'),
+	(r'^uapp/delete_app_version/$', 'delete_app_version'),
+	(r'^uapp/getAppReleases/(?P<appname>\w*)$', 'getAppReleases'),
+	(r'^uapp/getReleaseVersions/(?P<appname>\w*)/(?P<release>\w*)$', 'getReleaseVersions'),
+	(r'^uapp/getAppDetails/$', 'getAppDetails'),
+	(r'^uapp/getAppGroups/(?P<appname>\w*)/(?P<release>\w*)/(?P<version>\w*)$', 'getAppGroups'),
+	(r'^uapp/batchEditClients/(?P<validated>\w*)$', 'batchEditClients'),
+	
+	# Need to be re-written for data model change
+	(r'^uapp/logs/$', 'logs'),
+	(r'^uapp/get/(?P<imei>\w*)$', 'get'),
+	(r'^uapp/register/', 'register'),
+	(r'^uapp/$', 'index'),
+	
+	
+	
+	
+	
+	
     # Example:
     # (r'^updates/', include('updates.foo.urls')),
 
@@ -23,8 +49,8 @@ urlpatterns = patterns('updater.views',
 )
 
 urlpatterns += patterns('',
-    (r'^updater/login/$', 'django.contrib.auth.views.login',
+    (r'^uapp/login/$', 'django.contrib.auth.views.login',
         {'template_name': 'login.html'}),
-    (r'^updater/logout/$', 'django.contrib.auth.views.logout',
+    (r'^uapp/logout/$', 'django.contrib.auth.views.logout',
         {'template_name': 'login.html'}),
 )
